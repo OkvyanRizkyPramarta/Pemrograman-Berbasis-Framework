8fed55b09010c3e52f8ba37c2b79df096ae0aba1
OkvyanRizkyPramarta <pramarta26@gmail.com>
2022-03-20 23:21:46 +0700

    Praktikum Pertemuan 2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDom from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceworker';
 
-ReactDOM.render(
+const Hello = () => {
+  return <p>hello</p>
+}
+
+ReactDom.render (<Hello />, document.getElementById('root'));
+
+/*ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
@@ -14,4 +20,6 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals();*/
+
+serviceWorker.unregister();
