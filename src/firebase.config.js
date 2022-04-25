64bf9ca7b4f775de8ceeb1c1d44f85424255aa0b
OkvyanRizkyPramarta <pramarta26@gmail.com>
2022-04-25 07:06:28 +0700

    Praktikum Pertemuan 10

---
+++ src/firebase.config.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDz13BbDGDF_xzAGd7D8CSD6qZK91QFqjc",
+  authDomain: "project-firebase-260b7.firebaseapp.com",
+  projectId: "project-firebase-260b7",
+  storageBucket: "project-firebase-260b7.appspot.com",
+  messagingSenderId: "465928968891",
+  appId: "1:465928968891:web:c4a7b293c37832b85b679e",
+  measurementId: "G-VBZZRQ34L6"
+};
